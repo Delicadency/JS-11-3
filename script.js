cb52ec608fcf309ce26cd55fbc4d52ec3581b93a
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-18 12:02:51 +0200

    added validation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const axios = require("axios").default;
 const apiURL = "https://jsonplaceholder.typicode.com/posts";
 const input = document.createElement("input");
 input.setAttribute("data-test", "todo-input");
+input.setAttribute("name", "input");
 input.setAttribute("type", "text");
 document.body.appendChild(input);
 const button = document.createElement("button");
@@ -17,6 +18,12 @@ const addTodoElem = async () => {
     const postTodo = await axios.post(apiURL, { value: inputValue });
     console.log("Dodano nową wartość: ", postTodo.data);
   } catch {
+    const label = document.createElement("label");
+    label.setAttribute("for", "input");
+    label.setAttribute("data-test", "todo-input-label");
+    label.value = "Request error";
+    label.appendChild(input);
+    document.body.appendChild(label);
     console.error("Wystąpił błąd podczas dodawania nowej wartości:", error);
   }
 };
