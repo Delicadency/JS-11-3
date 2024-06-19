47e6414aae57e2fc8789e9128085ee8af9a4795d
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-19 12:01:22 +0200

    updated validation, button innerText added

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-const axios = require("axios").default;
 const apiURL = "https://jsonplaceholder.typicode.com/posts";
 const input = document.createElement("input");
 input.setAttribute("data-test", "todo-input");
 input.setAttribute("name", "input");
 input.setAttribute("type", "text");
+input.id = "input";
 document.body.appendChild(input);
+const label = document.createElement("label");
+label.setAttribute("for", "input");
+label.setAttribute("data-test", "todo-input-label");
+document.body.appendChild(label);
 const button = document.createElement("button");
 button.setAttribute("data-test", "todo-add");
 button.setAttribute("type", "submit");
+button.innerText = "Wyślij";
 document.body.appendChild(button);
 
 input.value = "hello";
 
 const addTodoElem = async () => {
   try {
+    if (label.innerText === "Request error") {
+      label.innerText = "";
+    }
     const inputValue = input.value;
     const postTodo = await axios.post(apiURL, { value: inputValue });
     console.log("Dodano nową wartość: ", postTodo.data);
-  } catch {
-    const label = document.createElement("label");
-    label.setAttribute("for", "input");
-    label.setAttribute("data-test", "todo-input-label");
-    label.value = "Request error";
-    label.appendChild(input);
-    document.body.appendChild(label);
+  } catch (error) {
+    label.innerText = "Request error";
     console.error("Wystąpił błąd podczas dodawania nowej wartości:", error);
   }
 };
